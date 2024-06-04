615432cb4f6f2bc7d278ad678f446e5c884f04ac
miyake13000 <miyake2020@s.okayama-u.ac.jp>
2024-06-04 12:51:36 +0900

    Restore fixed timeslice

--- rapictld/src/main.rs
+++ rapictld/src/main.rs
@@ -2,18 +2,7 @@ use clap::Parser;
 use log::debug;
 use serde::{Deserialize, Serialize};
 use simplelog::{Config, LevelFilter, SimpleLogger};
-use std::{
-    mem::size_of,
-    net::UdpSocket,
-    str::FromStr,
-    sync::{
-        atomic::{AtomicU32, Ordering},
-        Arc,
-    },
-    thread,
-    thread::sleep,
-    time::{Duration, Instant},
-};
+use std::{mem::size_of, net::UdpSocket, str::FromStr, thread, thread::sleep, time::Duration};
 
 const TIMESLICE_IN_COMM: Duration = Duration::from_millis(100);
 const TIMESLICE_GUARANTEED: Duration = Duration::from_millis(400);
@@ -44,7 +33,7 @@ struct Args {
     /// Duration (ms) between suspending and resuming job.
     /// If timeslice < 0, turn off job switching.
     #[arg(short = 't', long, required = true)]
-    _timeslice: i64,
+    timeslice: i64,
 
     /// Port to bind
     #[arg(short = 'p', long, default_value_t = DEFAULT_PORT)]
@@ -72,7 +61,6 @@ struct Data {
 
 fn main() {
     let args = Args::parse();
-    let count_in_communication = Arc::new(AtomicU32::new(0));
     SimpleLogger::init(
         LevelFilter::from_str(&args.debug).unwrap(),
         Config::default(),
@@ -81,90 +69,41 @@ fn main() {
 
     let socket = UdpSocket::bind((BIND_ADDR, args.port)).unwrap();
     let sender_socket = socket.try_clone().unwrap();
-    {
-        let count_in_communication = Arc::clone(&count_in_communication);
+
+    if args.timeslice > 0 {
+        let duration = Duration::from_millis(args.timeslice.try_into().unwrap());
         thread::spawn(move || {
-            recv_req_loop(socket, &count_in_communication).unwrap();
+            send_req(sender_socket, duration, &args.rapid_addrs, args.rapid_port).unwrap();
         });
-    }
-
-    let mut is_job_running = true;
-    let mut instant = Instant::now();
-    loop {
-        let elapsed = instant.elapsed();
-
-        if is_job_running {
-            if elapsed >= TIMESLICE_GUARANTEED
-                || count_in_communication.load(Ordering::Relaxed) > 0
-                    && elapsed >= TIMESLICE_IN_COMM
-            {
-                let stop_req = Data {
-                    req: REQ_STOP,
-                    dummy: 0,
-                };
-                send_req(
-                    &sender_socket,
-                    &stop_req,
-                    &args.rapid_addrs,
-                    args.rapid_port,
-                )
-                .unwrap();
-                is_job_running = false;
-                instant = Instant::now();
-            }
-        } else {
-            #[warn(clippy::collapsible_else_if)]
-            if elapsed >= TIMESLICE_IN_COMM {
-                let cont_req = Data {
-                    req: REQ_CONT,
-                    dummy: 0,
-                };
-                send_req(
-                    &sender_socket,
-                    &cont_req,
-                    &args.rapid_addrs,
-                    args.rapid_port,
-                )
-                .unwrap();
-                is_job_running = true;
-                instant = Instant::now();
-            }
-        }
-        sleep(TIMESLICE_CHECK_INTERVAL);
+        recv_req(socket).unwrap();
+    } else {
+        recv_req(socket).unwrap();
     }
 }
 
 fn send_req(
-    socket: &UdpSocket,
-    req: &Data,
-    addrs: &[String],
-    port: u16,
+    stream: UdpSocket,
+    duration: Duration,
+    nodes: &[String],
+    tport: u16,
 ) -> Result<(), std::io::Error> {
-    let buf = bincode::serialize(&req).unwrap();
-    for addr in addrs.iter() {
-        socket.send_to(&buf, (addr.as_str(), port))?;
-        debug!("Send request: {:?} to: {}", req, addr);
+    let mut req = FIRST_REQ;
+    loop {
+        let buf = bincode::serialize(&req).unwrap();
+        for host in nodes.iter() {
+            debug!("Send request: {:?} to: {}", req, host);
+            stream.send_to(&buf, (host.as_str(), tport))?;
+        }
+        reverse_request(&mut req).unwrap();
+        sleep(duration);
     }
-    Ok(())
 }
 
-fn recv_req_loop(
-    socket: UdpSocket,
-    count_in_communication: &AtomicU32,
-) -> Result<(), std::io::Error> {
+fn recv_req(stream: UdpSocket) -> Result<(), std::io::Error> {
     let mut buf: [u8; BUF_SIZE] = [0; BUF_SIZE];
     loop {
-        socket.recv(&mut buf)?;
+        stream.recv(&mut buf)?;
         let req: Data = bincode::deserialize(&buf).unwrap();
-        match req.req {
-            REQ_BEGIN_COMM => {
-                count_in_communication.fetch_add(1, Ordering::Relaxed);
-            }
-            REQ_END_COMM => {
-                count_in_communication.fetch_sub(1, Ordering::Relaxed);
-            }
-            _ => {}
-        };
         debug!("Receive request: {:?}", req);
     }
 }
